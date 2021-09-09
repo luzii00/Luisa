944be3685552c93f599ece808ede82898eb6fa5c
Brian Han <han12@llnl.gov>
2021-09-09 16:22:49 -0700

    WIP vtk fail trying to build virtual gl, mesa

--- scripts/uberenv/packages/geosx/package.py
+++ scripts/uberenv/packages/geosx/package.py
@@ -120,6 +120,7 @@ class Geosx(CMakePackage, CudaPackage):
     depends_on('pugixml@1.8: +shared')
 
     depends_on('fmt@8.0: +cxxstd=14 +pic')
+    depends_on('vtk@9.0.0')
 
     #
     # Math
@@ -379,8 +380,12 @@ class Geosx(CMakePackage, CudaPackage):
                        ('adiak', 'ADIAK', '+caliper' in spec),
                        ('caliper', 'CALIPER', '+caliper' in spec),
                        ('pugixml', 'PUGIXML', True),
+<<<<<<< HEAD
                        ('vtk', 'VTK', False)
                        ('fmt', 'FMT', True))
+=======
+                       ('vtk', 'VTK', True))
+>>>>>>> c48c12b0f (WIP vtk fail trying to build virtual gl, mesa)
             cfg.write('#{0}\n'.format('-' * 80))
             cfg.write('# IO TPLs\n')
             cfg.write('#{0}\n\n'.format('-' * 80))
